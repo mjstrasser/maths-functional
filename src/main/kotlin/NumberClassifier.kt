463df3aa199e751d65b54cf46d5ad105cbe49018
Michael Strasser <michael@michaelstrasser.com>
2020-10-24 16:04:53 +1000

    Functions are extensions to Int type

--- src/main/kotlin/NumberClassifier.kt
+++ src/main/kotlin/NumberClassifier.kt
@@ -3,13 +3,13 @@ package mjs.maths
 import arrow.core.Eval
 import kotlin.math.sqrt
 
-fun factorsOf(number: Int): List<Int> =
-    (1..sqrt(number.toDouble()).toInt() + 1)
-        .filter { number % it == 0 }
-        .run { this + this.map { number / it } }
+fun Int.factors(): List<Int> =
+    (1..sqrt(this.toDouble()).toInt() + 1)
+        .filter { this % it == 0 }
+        .let { facts -> facts + facts.map { fact -> this / fact } }
         .distinct()
         .sorted()
 
-fun aliquotSum(number: Int): Int = Eval.later {
-    factorsOf(number).sum() - number
+fun Int.aliquotSum(): Int = Eval.later {
+    this.factors().sum() - this
 }.value()
--- src/test/kotlin/NumberClassifierTests.kt
+++ src/test/kotlin/NumberClassifierTests.kt
@@ -7,31 +7,31 @@ class NumberClassifierTests : DescribeSpec({
 
     describe("calculating factors") {
         it("are only 1 for 1") {
-            factorsOf(1) shouldBe listOf(1)
+            1.factors() shouldBe listOf(1)
         }
         it("are 1 and 2 for 2") {
-            factorsOf(2) shouldBe listOf(1, 2)
+            2.factors() shouldBe listOf(1, 2)
         }
         it("are 1 and 3 for 3") {
-            factorsOf(3) shouldBe listOf(1, 3)
+            3.factors() shouldBe listOf(1, 3)
         }
         it("are 1, 2 and 4 for 4") {
-            factorsOf(4) shouldBe listOf(1, 2, 4)
+            4.factors() shouldBe listOf(1, 2, 4)
         }
         it("are 1, 2, 3, 4, 6 and 12 for 12") {
-            factorsOf(12) shouldBe listOf(1, 2, 3, 4, 6, 12)
+            12.factors() shouldBe listOf(1, 2, 3, 4, 6, 12)
         }
         it("are only 1 and 13 for 13") {
-            factorsOf(13) shouldBe listOf(1, 13)
+            13.factors() shouldBe listOf(1, 13)
         }
     }
 
     describe("Aliquot sum") {
         it("is 0 for 1") {
-            aliquotSum(1) shouldBe 0
+            1.aliquotSum() shouldBe 0
         }
         it("is 1 for 2") {
-            aliquotSum(2) shouldBe 1
+            2.aliquotSum() shouldBe 1
         }
         it("is correct for the first 50 values") {
             // From https://en.wikipedia.org/wiki/Aliquot_sum
@@ -39,7 +39,7 @@ class NumberClassifierTests : DescribeSpec({
                 1, 16, 1, 10, 9, 15, 1, 21, 1, 22,
                 11, 14, 1, 36, 6, 16, 13, 28, 1, 42,
                 1, 31, 15, 20, 13, 55, 1, 22, 17, 50,
-                1, 54, 1, 40, 33, 26, 1, 76, 8, 43).forEachIndexed { idx, sum -> aliquotSum(idx + 1) shouldBe sum }
+                1, 54, 1, 40, 33, 26, 1, 76, 8, 43).forEachIndexed { idx, sum -> (idx + 1).aliquotSum() shouldBe sum }
         }
     }
 
